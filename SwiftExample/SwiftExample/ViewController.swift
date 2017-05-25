afc985f841f4f78367cf5dbb2025d21969cbe1e6
shenfh <s15880277594@gmail.com>
2017-05-25 17:46:22 +0800

    提交示例文件

--- SwiftExample/SwiftExample/ViewController.swift
+++ SwiftExample/SwiftExample/ViewController.swift
@@ -25,25 +25,52 @@ class ViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationTitle("SwiftFrameWork")
-        leftNavigationButton(UIImage(named:"leftImage"), hlightedImage: UIImage(named:"leftImage"))
+//        leftNavigationButton(UIImage(named:"leftImage"), hlightedImage: UIImage(named:"leftImage"))
         rightNavigationButton(NSAttributedString(string: "right", attributes: [NSFontAttributeName : UIFont.systemFont(ofSize: 16)]))
        
-        leftNavButtonClosure = { void  in
-            printlen("leftNavButtonClosure")
-        }
+//        leftNavButtonClosure = { void  in
+//            printlen("leftNavButtonClosure")
+//        }
         rightNavButtonClosure = {void in
             printlen("rightNavButtonClosure")
+            let vc = ViewController();
+            vc.navigationBarHidden = true
+            self.navigationController?.pushViewController(vc, animated: true)
         }
         leftNavigationButton?.extendTouchInsets = UIEdgeInsetsMake(10, 0, 10, 200)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func prepareUI() {
+        super.prepareUI()
+        
+        let labe = UIButton()
+        labe.isUserInteractionEnabled = true
+        labe.backgroundColor = UIColor.green
+        labe.addClosureForEvent(event: .touchUpInside) { (sender:Any?) in
+            printlen("hahfhdhfhdhdhdhhd")
+        }
+        labe.addClosureForEvent(event: .touchDown) { (sender:Any?) in
+            printlen("touchDowntouchDown")
+        }
+//        labe.addTarget(self, action: #selector(targetAction(sender:)), for: .allTouchEvents)
+        self.view.addSubview(labe)
+        
+        labe.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[label]-0-|", options: NSLayoutFormatOptions(rawValue:0), metrics: nil, views: ["label":labe]))
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-68-[label(80)]", options: NSLayoutFormatOptions(rawValue:0), metrics: nil, views: ["label":labe]))
+        
+       
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func targetAction(sender:UIButton) {
+        printlen("55555555 \(sender.allControlEvents)")
+    }
 
 }
 
